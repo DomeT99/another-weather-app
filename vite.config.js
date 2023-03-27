a2a839bd43ca726d1719540739b21fd3e0fa49fa
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-27 15:08:50 +0200

    first commit and setup

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+      manifest: {
+        name: "Another Weather App",
+        short_name: "Another Weather App",
+        description: "The usual weather app but more beautiful!💫",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
