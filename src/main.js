962871475787abc214e65d098167064fa7efb09c
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-29 16:40:31 +0200

    feat: partial approach with geolocation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
 /**Router */
 import router from "./router/index.js";
 
 /**CSS*/
 import "bulma/css/bulma.css";
-import "./assets/scss/global.scss"
+import "./assets/scss/global.scss";
 
+const pinia = createPinia();
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount("#app");
---
+++ src/store/airPollutionStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { reactive, watch } from "vue";
+
+export const useAirPollutionStore = defineStore("air-pollution", () => {
+  const coordinates = reactive({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  watch(coordinates, () => getAirPollution());
+
+  function enableGeolocation() {
+    if (navigator.onLine) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        coordinates.latitude = position.coords.latitude;
+        coordinates.longitude = position.coords.longitude;
+      });
+    }
+  }
+
+  async function getAirPollution() {
+    try {
+      console.log(coordinates);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  return { enableGeolocation };
+});
