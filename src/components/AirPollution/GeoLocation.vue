a0990c4927492c2317a4cb6efe43644db99c227c
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-29 14:29:46 +0200

    feat:add active geolocation section

---
+++ src/components/AirPollution/GeoLocation.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Button from "../Button.vue";
+</script>
+<template>
+  <div class="columns mt-12">
+    <div class="column is-12 has-text-centered">
+      <h2>To check the air quality in your area, you need Geolocation.</h2>
+    </div>
+  </div>
+  <div class="columns mt-4">
+    <div class="column is-2 is-4-mobile is-offset-5 is-offset-4-mobile">
+      <Button class="is-tertiary b-sixth w-100 is-large">Active</Button>
+    </div>
+  </div>
+</template>
--- src/views/AirPollution.vue
+++ src/views/AirPollution.vue
@@ -1,2 +1,11 @@
-<script setup></script>
-<template></template>
+<script setup>
+import GeoLocation from "../components/AirPollution/GeoLocation.vue";
+
+//CO = Monossido di carbonio
+//NO2 = Biossido di azoto
+//O3 = Ozono
+//SO2 = Biossido di zolfo 
+</script>
+<template>
+  <GeoLocation></GeoLocation>
+</template>
