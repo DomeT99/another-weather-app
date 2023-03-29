f9c0c1a397112a465b0050062bf2e5c13be120b7
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-29 15:47:05 +0200

    feat: notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="notification is-secondary has-text-centered">
+    <p>Ops... something went wrong</p>
+  </div>
+</template>
--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,12 +1,19 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
+import Notification from "../components/Notification.vue";
 </script>
 
 <template>
   <Navbar></Navbar>
   <section class="container min-h-100">
     <slot></slot>
+
+    <div v-if="false" class="columns is-align-items-center min-h-30">
+      <div class="column is-4 is-offset-4">
+        <Notification></Notification>
+      </div>
+    </div>
   </section>
   <Footer></Footer>
 </template>
