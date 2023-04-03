b74f60c4e56e9d536de885cf61ed6bd50226ff7c
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-03 16:51:18 +0200

    test: usefetch testing

---
+++ tests/useFetch.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { useFetch } from "../src/composable/useFetch";
+
+describe("suite fetch composable", () => {
+  it("use fetch", () => {
+    const parameters = {
+      url: `${import.meta.env.VITE_URL_AIRPOLLUTION}?lat=${333}&lon=${343}`,
+      key: import.meta.env.VITE_KEY_AIRPOLLUTION,
+      host: import.meta.env.VITE_HOST_AIRPOLLUTION,
+    };
+
+    const response = useFetch(parameters);
+
+    expect(response).not.toBeUndefined();
+  });
+});
